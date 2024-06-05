bc469dc4593cd6e6b5a7555c5301e234ef94fcd5
faermanj <faermanj@users.noreply.github.com>
2024-06-05 15:27:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="CaravanaCloud"
+original_name="cli_gw"
+original_urlname="cli_gw"
+original_description="Awesome cli_gw created by CaravanaCloud"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv cli_gw $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ cli_gw/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for cli_gw."""
+
+from cli_gw.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
